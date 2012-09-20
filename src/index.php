02d60e1b235fe55e3057c73aabf554c15c17382e
Felippe da Motta Raposo <raposo.felippe@gmail.com>
2012-09-20 17:30:00 -0300

    comentei a parada dos grupos

--- src/index.php
+++ src/index.php
@@ -3,7 +3,7 @@
     
     $appId = '160032724121022'; //appid from facebook
     $secret = 'a8fa687e01d1eaa1c7b984223c764490'; //secret from facebook
-    $groupId = '130411277057132'; //facebook groupid
+    //$groupId = '130411277057132'; //facebook groupid
     
     $facebook = new Facebook(array(
       'appId'  => $appId,
@@ -37,11 +37,11 @@ $loginUrl = $facebook->getLoginUrl(array(
 echo '<a href="'.$loginUrl.'">Login with Facebook</a>';
 }
     
-    $response = $facebook->api('/'.$groupId.'/feed', array('limit' => 6, 'fields'=>'from,message,created_time'));    
+   /* $response = $facebook->api('/'.$groupId.'/feed', array('limit' => 6, 'fields'=>'from,message,created_time'));    
     print "<div class='facebook-feed-title'>Facebook Feed</div>";
     foreach ($response['data'] as $value) {
         print "<div class='facebook-from'><a href='http://www.facebook.com/home.php?#!/profile.php?id=".$value['from']['id']."'>".$value['from']['name']."</a> wrote:</div>";
         print "<div class='facebook-message'>".$value['message']."</div>";
         
-    }
+    }*/
 ?>
